f54862ecf0d06b12770785a73287887effb34db6
Marcos Mello <Marcosvinicius-vm@outlook.com>
2022-05-07 17:08:38 -0300

    Add base for the third list

---
+++ List3.c
@@ -0,0 +1,178 @@
+#include <stdio.h>
+#include <math.h>
+
+void e1(){
+
+}
+
+void e2(){
+
+}
+
+void e3(){
+
+}
+
+void e4(){
+
+}
+
+void e5(){
+
+}
+
+void e6(){
+
+}
+
+void e7(){
+
+}
+
+void e8(){
+
+}
+
+void e9(){
+
+}
+
+void e10(){
+
+}
+
+void e11(){
+
+}
+
+void e12(){
+
+}
+
+void e13(){
+
+}
+
+void e14(){
+
+}
+
+void e15(){
+
+}
+
+void e16(){
+
+}
+
+void e17(){
+
+}
+
+void e18(){
+
+}
+
+void e19(){
+
+}
+
+void e20(){
+
+}
+
+void e21(){
+
+}
+
+void e22(){
+
+}
+
+void e23(){
+
+}
+
+void e24(){
+
+}
+
+void e25(){
+
+}
+
+void e26(){
+
+}
+
+void e27(){
+
+}
+
+void e28(){
+
+}
+
+void e29(){
+
+}
+
+void e30(){
+
+}
+
+void e31(){
+
+}
+
+void e32(){
+
+}
+
+void e33(){
+
+}
+
+void e34(){
+
+}
+
+void e35(){
+
+}
+
+void e36(){
+
+}
+
+void e37(){
+
+}
+
+void e38(){
+
+}
+
+void e39(){
+
+}
+
+void e40(){
+
+}
+
+void e41(){
+
+}
+
+void e42(){
+
+}
+
+void e43(){
+
+}
+
+int main(){
+    return 0;
+}
