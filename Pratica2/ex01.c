7134571aafd064f9ae63e738884fb68e865dc7cd
Marcos Mello <Marcosvinicius-vm@outlook.com>
2022-05-18 17:04:47 -0300

    Pratica2

--- Pratica2/ex01.c
+++ Pratica2/ex01.c
@@ -5,35 +5,40 @@ int main(){
     float n1 = 0, n2 = 0;
 
     do{
-        printf("1 - Adicao;\n2 - Subtracao;\n3 - Multiplicacao;\n4 - Divisao;\n5 - Sair.");
+        printf("1 - Adicao;\n2 - Subtracao;\n3 - Multiplicacao;\n4 - Divisao;\n5 - Sair.\n");
         printf("Insira a operacao desejada: ");
         scanf("%d", &op);
 
-        if (op != 5){
+        if (op >= 1 && op < 5){
             printf("Insira os dois numeros separados por um espaco: ");
             scanf("%f %f", &n1, &n2);
 
             switch(op){
                 case 1:
-                    printf("Soma: %.2f", (n1+n2));
+                    printf("Soma: %.2f\n", (n1+n2));
                     n1 = 0;
                     n2 = 0;
 
                     break;
                 case 2:
-                    printf("Subtracao: %.2f", (n1-n2));
+                    printf("Subtracao: %.2f\n", (n1-n2));
                     n1 = 0;
                     n2 = 0;
 
                     break;
                 case 3:
-                    printf("Multiplicacao: %.2f", (n1*n2));
+                    printf("Multiplicacao: %.2f\n", (n1*n2));
                     n1 = 0;
                     n2 = 0;
 
                     break;
                 case 4:
-                    printf("Divisao: %.2f", (n1/n2));
+                    if (n2 != 0){
+                        printf("Divisao: %.2f\n", (n1/n2));
+                    }
+                    else{
+                        printf("Divisao por 0 é impossivel!\n");
+                    }
                     n1 = 0;
                     n2 = 0;
 
---
+++ Pratica2/ex04.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+
+int main(){
+    float preco = 0, precof = 0;
+    int pais = 0;
+    char transp = ' ', carg = ' ';
+
+    do{
+        printf("Digite o preco unitario: ");
+        scanf("%f", &preco);
+    }while(preco <= 0);
+    do{
+        printf("1 - Estados Unidos\n2 - Mexico\n3 - Outros\n");
+        printf("Digite o pais de origem: ");
+        scanf("%d", &pais);
+    }while(pais < 1 || pais > 3);
+    do{
+        printf("T - terrestre\nF - Fluvial\nA - aereo\n");
+        printf("Digite o meio de transporte: ");
+        scanf("%c", &transp);
+    }while(transp != 'T' && transp != 'F' && transp != 'A');
+    do{
+        printf("1 - Sim\nN - Nao\n");
+        printf("Carga Perigosa? ");
+        scanf("%c", &carg);
+    }while(carg != '1' && carg != 'N');
+
+    if (preco > 100){
+        printf("Imposto: R$ %.2f\n", (preco*0.1));
+        precof += (preco*1.1);
+    }
+    else{
+        printf("Imposto: R$ %.2f\n", (preco*0.05));
+        precof += (preco*1.05);
+    }
+
+    switch(pais){
+        case (1):
+            if (carg == '1'){
+                printf("Valor do transporte: R$ 50,00\n");
+                precof += 50;
+            }
+            else{
+                printf("Valor do transporte: R$ 12,00\n");
+                precof += 12;
+            }
+            break;
+        case (2):
+            if (carg == '1'){
+                printf("Valor do transporte: R$ 21,00\n");
+                precof += 21;
+            }
+            else{
+                printf("Valor do transporte: R$ 21,00\n");
+                precof += 21;
+            }
+            break;
+        case (3):
+            if (carg == '1'){
+                printf("Valor do transporte: R$ 24,00\n");
+                precof += 24;
+            }
+            else{
+                printf("Valor do transporte: R$ 60,00\n");
+                precof += 60;
+            }
+            break;
+    }
+
+    if (pais == 2 && transp == 'A'){
+        printf("Valor do seguro: R$ %.2f\n", (preco/2.0));
+        precof += preco/2.0;
+    }
+    else{
+        printf("Valor do seguro: R$ 0\n");
+    }
+
+    printf("Valor final: %.2f\n", precof);
+
+    printf("Valor total dos impostos: R$ %.2f", precof-preco);
+    //Como fiquei em duvida sobre o que era o imposto, fiz o preço total menos o unitario
+
+    return 0;
+}
