8f0f23daf6ef41e6b6cd94a75e24133b31e7b01e
Marcos Mello <Marcosvinicius-vm@outlook.com>
2022-07-08 14:02:45 -0300

    Add some code to be tested

--- BaseCode.c
+++ BaseCode.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
 int main(){
+    int **M = (int **)calloc(2, sizeof(int *));
+    for (int i = 0; i < 2; i++){
+        *(M+i) = (int *)calloc(2, sizeof(int));
+    }
+
+    for (int i = 0; i < 2; i++){
+        for (int j = 0; j < 2; j++){
+            printf("Insira o valor da posicao %d %d: ", i, j);
+            scanf("%d", (*(M+i)+j));
+        }
+    }
+
+    for (int i = 0; i < 2; i++){
+        for (int j = 0; j < 2; j++){
+            printf("%d ", *(*(M+i)+j));
+        }
+        printf("\n");
+    }
 
     return 0;
 }
